4ae65fc89cce5d177c9eba7529bfbf7b87b9cc2c
Guilherme Santos <guidev.codes@gmail.com>
2025-06-08 22:41:55 -0300

    General adjust of pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  root: 'src',
   build: {
+    outDir: '../dist',
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
-        clientes: path.resolve(__dirname, 'clientes.html'),
+        main: path.resolve(__dirname, 'src/index.html'),
+        clientes: path.resolve(__dirname, 'src/clientes.html'),
       },
     },
   },
