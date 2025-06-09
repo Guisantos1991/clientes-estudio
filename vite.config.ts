286f240b8b66f172f3d5a3e42865707d9a288cf4
Guilherme Santos <guidev.codes@gmail.com>
2025-06-08 23:08:22 -0300

    adjust

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  root: 'src',
   build: {
-    outDir: '../dist',
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'src/index.html'),
         clientes: path.resolve(__dirname, 'src/clientes.html'),
       },
     },
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 });
