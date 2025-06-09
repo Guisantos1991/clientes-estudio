7d2a1dd935d1f1917c55b8f974633bb14791b3dd
Guilherme Santos <guidev.codes@gmail.com>
2025-06-08 23:21:19 -0300

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  root: 'src',
   build: {
+    outDir: '../dist',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'src/index.html'),
         clientes: path.resolve(__dirname, 'src/clientes.html'),
       },
     },
-    outDir: 'dist',
-    emptyOutDir: true,
   },
 });
